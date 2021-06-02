fd183603f1524ba652980f68abdd3dec4609c025
CHWu <wuchin1997@gmail.com>
2021-06-02 08:17:57 +0200

    test

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        prefix: '!music',
+        token: 'ODQ5NTE3MDU4MTQwMTQzNjM2.YLcUaQ.a8NFGhOp-Qm93egHnHAJcfyJHFs',
+        activity: '小陈的蛋蛋',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
